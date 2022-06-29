fe95e1201d76c7ae2ddc34832f7a3d6d25ae50fe
wdse <95382758+whstime@users.noreply.github.com>
2022-06-29 14:45:55 +1200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from pyodide import create_proxy
 console.log("starting")
 toggleButton = document.getElementById('toggle')
 isBorderToggled = False
+##let jsBorderVar = false;
 def toggleBorders(event):
  console.log("toggled")
+ console.log("Variable isBorderToggled:", isBorderToggled)
+ ##console.log("JS Variable:", jsBorderVar)
  if isBorderToggled == False: 
   isBorderToggled = True
   for x in document.getElementsByClassName("command"):

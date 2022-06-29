143c8436451a596f4444f4a692df507586b13fee
wdse <95382758+whstime@users.noreply.github.com>
2022-06-29 14:59:41 +1200

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ console.log("starting")
 toggleButton = document.getElementById('toggle')
 isBorderToggled = False
 def toggleBorders(event):
- nonlocal isBorderToggled
  console.log("toggled")
  console.log("Variable isBorderToggled:", isBorderToggled)
- if isBorderToggled == False: 
+ if isBorderToggled == False:
+  nonlocal isBorderToggled
   isBorderToggled = True
   for x in document.getElementsByClassName("command"):
    x.style.border = "1px solid white"
  else:
+  nonlocal isBorderToggled
   isBorderToggled = False
   for x in document.getElementsByClassName("command"):
    x.style.border = "none"

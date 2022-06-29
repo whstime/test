76df931f6101e88e258fc0cbf23c71daf67b67c1
wdse <95382758+whstime@users.noreply.github.com>
2022-06-30 03:49:08 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 from pyodide import create_proxy
 console.log("starting")
-toggleButton = document.getElementById('toggle')
-isBorderToggled = False
 def toggleBorders(event):
  console.log("toggled")
- console.log("Variable isBorderToggled:", isBorderToggled)
- if isBorderToggled == False:
-  nonlocal isBorderToggled
-  isBorderToggled = True
-  for x in document.getElementsByClassName("command"):
-   x.style.border = "1px solid white"
- else:
-  nonlocal isBorderToggled
-  isBorderToggled = False
-  for x in document.getElementsByClassName("command"):
-   x.style.border = "none"
+ for x in document.getElementsByClassName("command"):
+    if x.style.outline == "none":
+     x.style.outline = "1px solid white"
+    else:
+     x.style.outline = "none"
+toggleButton = document.getElementById('toggle')
 tb = create_proxy(toggleBorders)
 toggleButton.addEventListener("click",tb)
